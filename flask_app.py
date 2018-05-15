8caa83d4e2fdac81edd98ffb21c0b6f32c52e3e7
Anna Fedoruk <annafedoruk.af@gmail.com>
2018-05-15 21:02:32 +0200

    commented msql & flask_mysqldb in flask_app!  + new menu

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
-from flask_mysqldb import MySQL
+# from flask_mysqldb import MySQL
 
 
 app = Flask(__name__)
-mysql = MySQL(app)
+# mysql = MySQL(app)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/search')
+def search_page():
+    return render_template('wyszukaj.html')
 
